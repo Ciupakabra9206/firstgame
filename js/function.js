b23130d52c6c46f54e8c1845accab35173b7ae44
Paweł Prorok <pawel3474@tlen.pl>
2021-11-05 18:49:19 +0100

    Create first move code

---
+++ js/function.js
@@ -0,0 +1,10 @@
+function printMessage(msg) {
+    let div = document.createElement('div');
+    div.innerHTML = msg;
+    document.getElementById('messages').appendChild(div);
+}
+
+function clearMessage() {
+    document.getElementById('messages').innerHTML = '';
+}
+
---
+++ js/script.js
@@ -0,0 +1,7 @@
+let computerMove = "kamień";
+
+printMessage("Zagrałem" + computerMove + "! Jeśli twój ruch to papier, to wygrywasz");
+
+let playerMove = "papier";
+
+printMessage("Zagrałem" + playerMove + "! Jeśli twój ruch to nożyczki, to wygrywasz");
